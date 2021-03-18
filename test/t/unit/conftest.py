e05312cb3ce8fa48eee8dc8f309f60249b304ec2
Kapil Badesara <badesara@amazon.com>
2021-03-17 17:25:14 -0700

    Removing redundant tests

--- test/t/unit/conftest.py
+++
@@ -1,9 +0,0 @@
-"""Setup unit test environment."""
-
-import sys
-import os
-
-# make sure tests can import the app code
-my_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, my_path + '/../../src/')
-
